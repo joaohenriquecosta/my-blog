06932d458e464e8ee9a2b8513552b4b1a085ca17
João Henrique Costa <joaohenrique_costa@icloud.com>
2022-10-21 03:33:13 -0300

    update layout.js and add utils.module.css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,20 +1,18 @@
-
-import Link from 'next/link';
 import Head from 'next/head';
-import Script from 'next/script';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
           <Head>
             <title>First Post</title>
           </Head>
           <h1>First Post</h1>
           <h2>
-    {/* The Link component enables client-side navigation between two pages in the same Next.js app.*/}
+    {/* The Link component enables client-side navigation between two pages in the same Next.js app.
             <Link href="/">Back to home</Link>
-    {/*Client-side navigation means that the page transition happens using JavaScript, which is faster than the default navigation done by the browser. */}
+    Client-side navigation means that the page transition happens using JavaScript, which is faster than the default navigation done by the browser. */}
           </h2>
-        </>
+        </Layout>
       );
   }
